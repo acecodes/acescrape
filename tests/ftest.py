6c6307d4d0fc7d21f72e0a69a13c69298d7965b7
Ace <real.deal.ace@gmail.com>
2014-04-24 13:34:23 -0700

    Minor tweaks to content

--- tests/ftest.py
+++ tests/ftest.py
@@ -3,12 +3,15 @@ from selenium import webdriver
 import unittest
 import time
 
+server = 'http://localhost:5000/'
+live_server = 'http://acescrape.herokuapp.com'
+
 class FuncTest(unittest.TestCase):
 	
 	def test_server_up(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-		self.browser.get('http://localhost:5000/')
+		self.browser.get(server)
 		time.sleep(5) # Use for degugging connection problems
 		self.browser.quit()
 
