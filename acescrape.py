707b0e7c3167da9365253bfeb1f40aaec46ed916
Ace <real.deal.ace@gmail.com>
2014-04-24 16:31:18 -0700

    Added Foundation navbar, first scrape of Reddit front page

--- acescrape.py
+++ acescrape.py
@@ -4,25 +4,27 @@ import urllib.request
 import re
 from flask import Flask
 from flask import render_template
+from jinja2 import Template
 
-# Scraping parameters
 class site_to_be_scraped:
 	def __init__(self, url):
 		self.url = url
-		data = urllib.request.urlopen(url)
-		soup = BeautifulSoup(data)
-		body = soup.get_text()
+		self.data = urllib.request.urlopen(url)
+		self.soup = BeautifulSoup(self.data)
+		self.body = self.soup.get_text()
+
+	def regex(self, string):
+		return re.findall(string, self.body)
 
 Reddit = site_to_be_scraped('http://www.reddit.com')
-
+my_string = Reddit.regex(r'/r/[a-z]+[^1234567890][^comments][^share]')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def front_page(name='AceScrape'):
-	return render_template('index.html', name=name)
-
-
+	return render_template('index.html', my_string=my_string, name=name)
 
 if __name__ == '__main__':
 	app.run(debug=True)
