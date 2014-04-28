4327764d1bb593fdd5f6dec4e5f3ddfeac987179
Ace <real.deal.ace@gmail.com>
2014-04-28 00:15:04 -0700

    Added new conditionals, Reddit cuteness index, other CSS tweaks

--- acescrape.py
+++ acescrape.py
@@ -23,9 +23,14 @@ Reddit = site_to_be_scraped('http://www.reddit.com')
 subreddits_original = Reddit.soup.find_all("a", class_="subreddit hover may-blank")
 # Placed into a set to avoid duplicates (since sets do not allow multiple occurances of elements)
 subreddits = set(subreddits_original)
-
 # Front page images
 images = Reddit.soup.find_all("a", class_="thumbnail")
+# Cuteness Index
+cuteness_finder = Reddit.regex(r'aww')
+cuteness_index_original = len(subreddits_original)/len(cuteness_finder)
+cuteness_index = str(cuteness_index_original)
+cuteness_index = cuteness_index[0:3]
+cuteness_levels = ['Filthy sloth', 'Excited corgi', 'Kitten euphoria']
 
 ## TechCrunch ##
 TechCrunch = site_to_be_scraped('http://www.techcrunch.com')
@@ -33,13 +38,20 @@ TechCrunch = site_to_be_scraped('http://www.techcrunch.com')
 # Number of times VCs are mentioned on TC's front page
 vc_word_search = TechCrunch.regex(r'VC[s]?')
 VCs = len(vc_word_search)
+disruption_levels = ['2014 MySpace', 'Getting Googley', 'Twittergasm']
+
+
+## Site ##
+tagline = "Scraping only the finest data"
 
 app = Flask(__name__)
 
 
+
+
 @app.route('/')
 def front_page(name='AceScrape'):
-	return render_template('index.html', subreddits=subreddits, name=name, images=images, VCs=VCs)
+	return render_template('index.html', subreddits=subreddits, name=name, images=images, VCs=VCs, disruption_levels=disruption_levels, cuteness_levels=cuteness_levels, cuteness_index=cuteness_index, cuteness_index_original=cuteness_index_original, tagline=tagline)
 
 if __name__ == '__main__':
 	app.run(debug=True)
