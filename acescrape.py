5812b80bace6af80e1a4d3dc3d93daea6ff136d5
Ace <real.deal.ace@gmail.com>
2014-04-27 18:14:16 -0700

    Added TechCrunch section, further refined CSS

--- acescrape.py
+++ acescrape.py
@@ -16,10 +16,9 @@ class site_to_be_scraped:
 	def regex(self, string):
 		return re.findall(string, self.body)
 
-# Instances
+## Reddit ##
 Reddit = site_to_be_scraped('http://www.reddit.com')
 
-## Reddit ##
 # Front page subreddits
 subreddits_original = Reddit.soup.find_all("a", class_="subreddit hover may-blank")
 # Placed into a set to avoid duplicates (since sets do not allow multiple occurances of elements)
@@ -28,13 +27,19 @@ subreddits = set(subreddits_original)
 # Front page images
 images = Reddit.soup.find_all("a", class_="thumbnail")
 
+## TechCrunch ##
+TechCrunch = site_to_be_scraped('http://www.techcrunch.com')
+
+# Number of times VCs are mentioned on TC's front page
+vc_word_search = TechCrunch.regex(r'VC[s]?')
+VCs = len(vc_word_search)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def front_page(name='AceScrape'):
-	return render_template('index.html', subreddits=subreddits, name=name, images=images)
+	return render_template('index.html', subreddits=subreddits, name=name, images=images, VCs=VCs)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- static/js/acescrape.js
+++ static/js/acescrape.js
@@ -1,9 +1,21 @@
 // Change script for Reddit box
 $(document).ready(function () {
     $('.reddit').hide();
-    $('#option1').show();
-    $('#selectField').change(function () {
+    $('#reddit1').show();
+    $('#selectField-reddit').change(function () {
         $('.reddit').hide();
         $('#'+$(this).val()).show();
     });
 });
+
+// Change script for TechCrunch box
+$(document).ready(function () {
+    $('.techcrunch').hide();
+    $('#techcrunch1').show();
+    $('#selectField-techcrunch').change(function () {
+        $('.techcrunch').hide();
+        $('#'+$(this).val()).show();
+    });
+});
+
+
